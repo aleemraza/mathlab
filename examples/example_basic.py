7f6a54d3ac276b9811a365c1aa37948cdf8b13b2
aleemraza <aleemraza661@gmail.com>
2025-09-30 21:08:31 +0800

    Initial commit: added mathlab library modules, __init__.py, and README with installation steps

---
+++ examples/example_basic.py
@@ -0,0 +1,2 @@
+from mathlab import add
+print("Add :" , add(2,2))
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="mathlab",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A lightweight Python math library for arithmetic, algebra, geometry, and number theory.",
+    author="Muhammad Aleem Raza",
+    author_email="aleemraza661@gmail.com",
+    python_requires=">=3.8",
+)
